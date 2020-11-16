68c69f485ce1fb6b141fd83cea7880e41f1e3969
MalenkovaMaria <antkilllller@gmail.com>
2020-11-16 17:20:04 +0300

    task-1 first commit

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+document.querySelector('.page').classList.add('classic'); // добавляет эл-ту с классом page класс classic
+console.log(document.querySelector('.page'))
+//document.querySelector('.page').classList.remove('classic');  удаляет у эл-та с классом page класс classic
