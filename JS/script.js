2fe465bd56c2de87f98a86d2314c80e5be9c19cd
MalenkovaMaria <antkilllller@gmail.com>
2021-01-10 18:50:15 +0300

    task-6

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-// task-6
+// task-6 1
 let factorial = (n) => {
     fact = 1
     for (let i = 1; i <= n; i++){
@@ -6,4 +6,19 @@ let factorial = (n) => {
     }
     return fact
 }
-alert(factorial(3))
+console.log(factorial(3))
+
+//2
+
+let sumSquareDifference = (n) => {
+    fact = 0
+    for (let i = 1; i <= n; i++){
+        fact += i**2 
+    }
+    k = 0
+    for (let i = 1; i <= n; i++){
+        k += i
+    }
+    return (k**2 - fact)
+}
+console.log(sumSquareDifference(10))
