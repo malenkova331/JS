2288f1d865e76fe52f7712f58fe2833f36444c8f
MalenkovaMaria <antkilllller@gmail.com>
2021-01-10 18:38:03 +0300

    task-6

--- JS/script.js
+++ JS/script.js
@@ -1,49 +1,9 @@
-
-
-// 1
-
-let smallestDivisor = function(x) {
-    let divisor = 0;
-    let a = 2;
-    let smallest = x;
-    while (a <= x/2){
-        if (x % a == 0){
-            divisor = a;
-            if (divisor < smallest){
-                smallest = divisor
-            }
-        }
-        a += 1;
-    };
-    if (smallest > 0){
-        return smallest;
-    } else {
-        return NaN
+// task-6
+let factorial = (n) => {
+    fact = 1
+    for (let i = 1; i <= n; i++){
+        fact = fact * i
     }
-}; 
-console.log(smallestDivisor(-1));
-
-
-// 2 
-
-let isPrime = function(x){
-    if (x == 1){
-        return false
-    };
-
-    let divisor = x;
-    let a = 2;
-    while (a <= x/2){
-        if (x % a == 0){
-            divisor = a;
-        }
-        a += 1;
-    };
-    if (divisor != x){
-        return false;
-    } else if(divisor != 1 & divisor == x){
-        return true;
-    };
-};
-
-console.log(isPrime(23))
+    return fact
+}
+alert(factorial(3))
